0ceab4d72ed6dd3bd4c57412eb95901a1aab8eb4
Nomad Insurance <team@nomadinsurance.com>
2025-03-02 13:49:43 -0600

    before web integration

--- apps/web/env.ts
+++ apps/web/env.ts
@@ -5,6 +5,7 @@ import { keys as core } from '@repo/next-config/keys';
 import { keys as observability } from '@repo/observability/keys';
 import { keys as rateLimit } from '@repo/rate-limit/keys';
 import { keys as security } from '@repo/security/keys';
+import { keys as supabase } from '@repo/supabase/keys';
 import { createEnv } from '@t3-oss/env-nextjs';
 
 export const env = createEnv({
@@ -16,6 +17,7 @@ export const env = createEnv({
     flags(),
     security(),
     rateLimit(),
+    supabase(),
   ],
   server: {},
   client: {},
