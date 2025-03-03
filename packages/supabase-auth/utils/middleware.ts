ff2c2eb45bf4720a3388f7337d3b4cfbd5d9a371
Nomad Insurance <justinbarsketis@gmail.com>
2025-03-03 13:13:48 -0600

    middleware redundancy

--- packages/supabase-auth/utils/middleware.ts
+++ packages/supabase-auth/utils/middleware.ts
@@ -1,6 +1,5 @@
-import { createServerClient } from '@supabase/ssr';
 import { type NextRequest, NextResponse } from 'next/server';
-import { keys } from '../keys';
+import { createServerClientForMiddleware } from '../clients/server';
 
 /**
  * Update the Supabase auth session in a Next.js middleware context
@@ -22,43 +21,23 @@ import { keys } from '../keys';
  * @returns NextResponse with the updated session
  */
 export async function updateSession(request: NextRequest) {
-  let supabaseResponse = NextResponse.next({
+  const response = NextResponse.next({
     request,
   });
 
-  const env = keys();
-
-  const supabase = createServerClient(
-    env.NEXT_PUBLIC_SUPABASE_URL,
-    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        setAll(cookiesToSet) {
-          for (const { name, value, options } of cookiesToSet) {
-            request.cookies.set(name, value);
-          }
-          supabaseResponse = NextResponse.next({
-            request,
-          });
-          for (const { name, value, options } of cookiesToSet) {
-            supabaseResponse.cookies.set(name, value, options);
-          }
-        },
-      },
-    }
-  );
+  // Use our middleware client instead of direct createServerClient
+  const supabase = createServerClientForMiddleware(request, response);
 
   // Do not run code between createServerClient and
   // supabase.auth.getUser(). A simple mistake could make it very hard to debug
   // issues with users being randomly logged out.
 
   // IMPORTANT: DO NOT REMOVE auth.getUser()
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  //   const {
+  //     data: { user },
+  //   } = await supabase.auth.getUser();
+
+  await supabase.auth.getUser();
 
   // You can optionally return a response based on the user's authentication status
   // Uncomment the following code to enable redirection to a login page for unauthenticated users
@@ -75,18 +54,5 @@ export async function updateSession(request: NextRequest) {
   }
   */
 
-  // IMPORTANT: You *must* return the supabaseResponse object as is.
-  // If you're creating a new response object with NextResponse.next() make sure to:
-  // 1. Pass the request in it, like so:
-  //    const myNewResponse = NextResponse.next({ request })
-  // 2. Copy over the cookies, like so:
-  //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
-  // 3. Change the myNewResponse object to fit your needs, but avoid changing
-  //    the cookies!
-  // 4. Finally:
-  //    return myNewResponse
-  // If this is not done, you may be causing the browser and server to go out
-  // of sync and terminate the user's session prematurely!
-
-  return supabaseResponse;
+  return response;
 }
