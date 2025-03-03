72329838bb38ac59f3eb755701de94675ae21a1f
Nomad Insurance <justinbarsketis@gmail.com>
2025-03-02 21:20:16 -0600

    not sure

--- apps/app/env.ts
+++ apps/app/env.ts
@@ -7,7 +7,7 @@ import { keys as core } from '@repo/next-config/keys';
 import { keys as notifications } from '@repo/notifications/keys';
 import { keys as observability } from '@repo/observability/keys';
 import { keys as security } from '@repo/security/keys';
-import { keys as supabase } from '@repo/supabase/keys';
+import { keys as supabase } from '@repo/supabase-auth/keys';
 import { keys as webhooks } from '@repo/webhooks/keys';
 import { createEnv } from '@t3-oss/env-nextjs';
 
